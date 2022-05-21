b8042e2a3ba7cd141f280b5d83096c127d41cf08
samuVillegas <villegassamuel25@gmail.com>
2022-05-21 16:51:54 -0500

    Eliminacion de dist

--- dist/utilities/validator.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_joi_validation_1 = require("express-joi-validation");
-const validator = express_joi_validation_1.createValidator();
-exports.default = validator;
-//# sourceMappingURL=validator.js.map
